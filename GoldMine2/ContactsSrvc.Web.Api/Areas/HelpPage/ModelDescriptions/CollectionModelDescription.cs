468233dccd03991bcd302bd99321662f2e015b27
mindsqual <parrybp@yahoo.com>
2017-06-16 10:14:05 -0400

    significant work with Database Project.  Created new Web.Api project TO-DO: Add rest of projects

---
+++ GoldMine2/ContactsSrvc.Web.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ContactsSrvc.Web.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
