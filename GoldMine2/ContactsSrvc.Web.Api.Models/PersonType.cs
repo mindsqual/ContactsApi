c806b56a3827fff368b4462ee2f1b249a038ecc9
mindsqual <parrybp@yahoo.com>
2017-06-16 21:03:22 -0400

    Added Web.Api.Models classes

---
+++ GoldMine2/ContactsSrvc.Web.Api.Models/PersonType.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ContactSrvc.Web.Api.Models
+{
+    /// <summary>
+    /// A web resource type that represents 
+    /// a PersonType (classification of person in the database
+    /// </summary>
+    class PersonType
+    {
+        public long PersonTypeId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Link> Links { get; set; }
+    }
+}
